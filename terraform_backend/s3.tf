58a3d69fd36d28477a1e2476d556acb8fd80993e
Trung <trung191997@gmail.com>
2024-03-21 20:41:36 +0700

    test

--- terraform_backend/s3.tf
+++ terraform_backend/s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "qa-community-${var.your_account}-backend-s3"
+  bucket = "${var.your_account}"
   lifecycle {
     prevent_destroy = true
   }
